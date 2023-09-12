cf1d46aa6108e139905fc8b021baaa83bed346bc
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-09-12 11:38:58 -0300

    add google auth

---
+++ src/components/buttons.component.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { signIn, signOut } from "next-auth/react";
+import Link from "next/link";
+
+export const LoginButton = () => {
+  return (
+    <button style={{ marginRight: 10 }} onClick={() => signIn()}>
+      Sign in
+    </button>
+  );
+};
+
+export const LogoutButton = () => {
+  return (
+    <button style={{ marginRight: 10 }} onClick={() => signOut()}>
+      Sign Out
+    </button>
+  );
+};
---
+++ src/lib/auth.ts
@@ -0,0 +1,37 @@
+import type { NextAuthOptions } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+  session: {
+    strategy: "jwt",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+  ],
+  callbacks: {
+    session: ({ session, token }) => {
+      // get user fom the API
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          randomKey: "test",
+        },
+      };
+    },
+    jwt: ({ token, user }) => {
+      if (user) {
+        const u = user as unknown as any;
+        return {
+          ...token,
+          id: u.id,
+        };
+      }
+      return token;
+    },
+  },
+};
