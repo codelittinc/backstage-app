68d5fa31a3b3edfe78ccc657a28450fb53d52c35
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-09-13 11:46:19 -0300

    add users authentication

--- src/app/users/sign-in/page.tsx
+++ src/app/users/sign-in/page.tsx
@@ -1,17 +1,33 @@
 "use client";
-import { User } from "@/components/user.component";
-import type { InferGetServerSidePropsType } from "next";
 import { signIn } from "next-auth/react";
+import Box from "@/components/Box";
+import Button from "@/components/Button";
+import IllustrationLayout from "./IllustrationLayout";
 
-export default function SignIn() {
+import bgImage from "@/assets/images/illustrations/illustration-reset.jpg";
+
+function Illustration(): JSX.Element {
   return (
-    <>
-      <div>
-        <button onClick={() => signIn("google", { callbackUrl: "/dashboard" })}>
-          Sign in with Google
-        </button>
-        <User />
-      </div>
-    </>
+    <IllustrationLayout
+      title="Welcome to Codelitt Backstage!"
+      description="Sign in with your Codelitt account to continue."
+      illustration={bgImage.src}
+    >
+      <Box component="form" role="form">
+        <Box mt={4} mb={1}>
+          <Button
+            variant="gradient"
+            color="info"
+            size="large"
+            fullWidth
+            onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
+          >
+            Sign in with Google
+          </Button>
+        </Box>
+      </Box>
+    </IllustrationLayout>
   );
 }
+
+export default Illustration;
--- src/components/buttons.component.tsx
+++ src/components/buttons.component.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { signIn, signOut } from "next-auth/react";
-import Link from "next/link";
+//import Link from "next/link";
 
 export const LoginButton = () => {
   return (
