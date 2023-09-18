65329aa9c893b5cdc0d71a6c5c729868e4fdec24
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-09-18 11:09:00 -0300

    add CT DashboardNavbar

---
+++ src/components/DashboardNavbar/index.tsx
@@ -0,0 +1,202 @@
+import { usePathname } from "next/navigation";
+import { useState, useEffect } from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import Icon from "@mui/material/Icon";
+import Box from "@/components/Box";
+import Input from "@/components/Input";
+import Badge from "@/components/Badge";
+import Breadcrumbs from "@/components/Breadcrumbs";
+import NotificationItem from "@/components/Items/NotificationItem";
+
+import {
+  navbar,
+  navbarContainer,
+  navbarRow,
+  navbarIconButton,
+  navbarDesktopMenu,
+  navbarMobileMenu,
+} from "./styles";
+
+import {
+  useMaterialUIController,
+  setTransparentNavbar,
+  setMiniSidenav,
+  setOpenConfigurator,
+} from "@/theme";
+import Link from "next/link";
+
+interface Props {
+  absolute?: boolean;
+  light?: boolean;
+  isMini?: boolean;
+}
+
+function DashboardNavbar({ absolute, light, isMini }: Props): JSX.Element {
+  const [navbarType, setNavbarType] = useState<
+    "fixed" | "absolute" | "relative" | "static" | "sticky"
+  >();
+  const [controller, dispatch] = useMaterialUIController();
+  const {
+    miniSidenav,
+    transparentNavbar,
+    fixedNavbar,
+    openConfigurator,
+    darkMode,
+  } = controller;
+  const [openMenu, setOpenMenu] = useState<any>(false);
+  const route = usePathname().split("/").slice(1);
+
+  useEffect(() => {
+    // Setting the navbar type
+    if (fixedNavbar) {
+      setNavbarType("sticky");
+    } else {
+      setNavbarType("static");
+    }
+
+    // A function that sets the transparent state of the navbar.
+    function handleTransparentNavbar() {
+      setTransparentNavbar(
+        dispatch,
+        (fixedNavbar && window.scrollY === 0) || !fixedNavbar
+      );
+    }
+
+    /** 
+     The event listener that's calling the handleTransparentNavbar function when 
+     scrolling the window.
+    */
+    window.addEventListener("scroll", handleTransparentNavbar);
+
+    // Call the handleTransparentNavbar function to set the state with the initial value.
+    handleTransparentNavbar();
+
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("scroll", handleTransparentNavbar);
+  }, [dispatch, fixedNavbar]);
+
+  const handleMiniSidenav = () => setMiniSidenav(dispatch, !miniSidenav);
+  const handleConfiguratorOpen = () =>
+    setOpenConfigurator(dispatch, !openConfigurator);
+  const handleOpenMenu = (event: any) => setOpenMenu(event.currentTarget);
+  const handleCloseMenu = () => setOpenMenu(false);
+
+  // Render the notifications menu
+  const renderMenu = () => (
+    <Menu
+      anchorEl={openMenu}
+      anchorReference={null}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left",
+      }}
+      open={Boolean(openMenu)}
+      onClose={handleCloseMenu}
+      sx={{ mt: 2 }}
+    >
+      <NotificationItem icon={<Icon>email</Icon>} title="Check new messages" />
+      <NotificationItem
+        icon={<Icon>podcasts</Icon>}
+        title="Manage Podcast sessions"
+      />
+      <NotificationItem
+        icon={<Icon>shopping_cart</Icon>}
+        title="Payment successfully completed"
+      />
+    </Menu>
+  );
+
+  // Styles for the navbar icons
+  const iconsStyle = ({
+    palette: { dark, white, text },
+    functions: { rgba },
+  }: {
+    palette: any;
+    functions: any;
+  }) => ({
+    color: () => {
+      let colorValue = light || darkMode ? white.main : dark.main;
+
+      if (transparentNavbar && !light) {
+        colorValue = darkMode ? rgba(text.main, 0.6) : text.main;
+      }
+
+      return colorValue;
+    },
+  });
+
+  return (
+    <AppBar
+      position={absolute ? "absolute" : navbarType}
+      color="inherit"
+      sx={(theme) =>
+        navbar(theme, { transparentNavbar, absolute, light, darkMode })
+      }
+    >
+      <Toolbar sx={navbarContainer}>
+        <Box
+          color="inherit"
+          mb={{ xs: 1, md: 0 }}
+          sx={(theme) => navbarRow(theme, { isMini })}
+        >
+          <Breadcrumbs
+            icon="home"
+            title={route[route.length - 1]}
+            route={route}
+            light={light}
+          />
+        </Box>
+        {isMini ? null : (
+          <Box sx={(theme) => navbarRow(theme, { isMini })}>
+            <Box pr={1}>
+              <Input label="Search here" />
+            </Box>
+            <Box color={light ? "white" : "inherit"}>
+              <Link href="/authentication/sign-in/basic">
+                <IconButton sx={navbarIconButton} size="small" disableRipple>
+                  <Icon sx={iconsStyle}>account_circle</Icon>
+                </IconButton>
+              </Link>
+              <IconButton
+                size="small"
+                disableRipple
+                color="inherit"
+                sx={navbarMobileMenu}
+                onClick={handleMiniSidenav}
+              >
+                <Icon sx={iconsStyle} fontSize="medium">
+                  {miniSidenav ? "menu_open" : "menu"}
+                </Icon>
+              </IconButton>
+              <IconButton
+                size="small"
+                disableRipple
+                color="inherit"
+                sx={navbarIconButton}
+                onClick={handleConfiguratorOpen}
+              >
+                <Icon sx={iconsStyle}>settings</Icon>
+              </IconButton>
+              <IconButton
+                size="small"
+                color="inherit"
+                sx={navbarIconButton}
+                onClick={handleOpenMenu}
+              >
+                <Badge badgeContent={9} color="error" size="xs" circular>
+                  <Icon sx={iconsStyle}>notifications</Icon>
+                </Badge>
+              </IconButton>
+              {renderMenu()}
+            </Box>
+          </Box>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default DashboardNavbar;
