305340a2617c33df75953a95670bf1417377e5ac
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-10-04 09:42:57 -0300

    create custom autocomplete

---
+++ src/components/Autocomplete/index.tsx
@@ -0,0 +1,49 @@
+import FormField from "@/app/users/settings/FormField";
+import { Autocomplete as MUIAutocomplete } from "@mui/material";
+
+interface AutocompleteProps {
+  value?: any;
+  defaultValue?: any;
+  onChange: (value: any) => void;
+  options: any[];
+  getOptionLabel?: (value: any) => string;
+  label: string;
+}
+
+const Autocomplete = ({
+  value,
+  onChange,
+  options,
+  getOptionLabel = (value: any) => value,
+  label,
+}: AutocompleteProps) => {
+  const handleChange = (_: any, newValue: any) => {
+    onChange(newValue);
+  };
+
+  return (
+    <MUIAutocomplete
+      getOptionLabel={getOptionLabel}
+      value={value}
+      options={options}
+      renderOption={(props, option) => (
+        <li
+          {...props}
+          key={typeof option != "string" ? JSON.stringify(option) : option}
+        >
+          {getOptionLabel(option)}
+        </li>
+      )}
+      renderInput={(params) => (
+        <FormField
+          {...params}
+          label={label}
+          InputLabelProps={{ shrink: true }}
+        />
+      )}
+      onChange={handleChange}
+    />
+  );
+};
+
+export default Autocomplete;
