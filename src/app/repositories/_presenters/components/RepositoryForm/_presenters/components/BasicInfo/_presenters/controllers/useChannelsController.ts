774fc99d02b5ea14422f5733e57f5341aaa0c00c
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-10-13 12:34:13 -0300

    fetch channels by customer

--- src/app/repositories/_presenters/components/RepositoryForm/_presenters/components/BasicInfo/_presenters/controllers/useChannelsController.ts
+++ src/app/repositories/_presenters/components/RepositoryForm/_presenters/components/BasicInfo/_presenters/controllers/useChannelsController.ts
@@ -1,12 +1,27 @@
 import { CHANNELS_KEY } from "@/app/repositories/_domain/constants";
 import { useQuery } from "@tanstack/react-query";
 import { getChannels } from "../data/services/channels";
+import { useEffect } from "react";
+import { useAppStore } from "@/app/_presenters/data/store/store";
 
-const useChannelsController = () => {
-  const { data, isLoading } = useQuery({
-    queryKey: [CHANNELS_KEY],
-    queryFn: getChannels,
+const useChannelsController = (customer: Customer) => {
+  const { showAlert } = useAppStore();
+  const { data, isLoading, isError } = useQuery({
+    queryKey: [CHANNELS_KEY, customer?.id],
+    queryFn: () => getChannels(customer),
+    enabled: !!customer,
+    retry: false,
   });
+
+  useEffect(() => {
+    if (isError) {
+      showAlert({
+        title: "Notifications token",
+        content: `The notifications token for ${customer?.name} is invalid! Please review it on the customer page.`,
+        color: "error",
+      });
+    }
+  }, [isError, customer, showAlert]);
 
   return {
     channels: data,
--- src/app/repositories/_presenters/components/RepositoryForm/_presenters/components/BasicInfo/_presenters/data/services/channels/index.ts
+++ src/app/repositories/_presenters/components/RepositoryForm/_presenters/components/BasicInfo/_presenters/data/services/channels/index.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
 import { getRoadrunnerUrl } from "../../../../../../../../../../../../api";
 
-export const getChannels = async () => {
-  const { data } = await axios.get(getRoadrunnerUrl("/channels.json"));
+export const getChannels = async (customer: Customer) => {
+  const { data } = await axios.get(
+    "https://api.notifications.codelitt.dev/api/channels",
+    {
+      headers: {
+        Authorization: customer.notificationsToken,
+      },
+    }
+  );
   return data;
 };
