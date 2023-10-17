3b187057af13cfbf06ce5b6d51c7b26130e8c2a4
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-10-17 10:47:12 -0300

    improve layout

--- src/components/LayoutContainers/DashboardLayout/index.tsx
+++ src/components/LayoutContainers/DashboardLayout/index.tsx
@@ -39,7 +39,7 @@ function DashboardLayout({ children, stickyNavbar }: Props): JSX.Element {
         })}
       >
         <DashboardNavbar absolute={!stickyNavbar} isMini />
-        <Box mt={10} />
+        <Box mt={5} />
         {children}
         <Footer />
       </Box>
