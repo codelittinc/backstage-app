f8749761eb6669cef3ef69d3f5f10781509d0b21
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-10-26 16:11:29 -0300

    add permissions to statements of work

--- src/app/projects/_presenters/components/ProjectForm/index.tsx
+++ src/app/projects/_presenters/components/ProjectForm/index.tsx
@@ -1,11 +1,13 @@
 "use client";
 import Grid from "@mui/material/Grid";
 import { useState } from "react";
-import FormLayout from "@/components/LayoutContainers/FormLayout";
 import Header from "./_presenters/components/Header";
 import BasicInfo from "./_presenters/components/BasicInfo";
 import SidenavForm from "@/components/SidenavForm";
 import StatementsOfWork from "./_presenters/components/StatementsOfWork";
+import usePermissions from "@/components/ProtectedComponent/_presenters/controllers/usePermissionsController";
+import { abilities, targets } from "@/permissions";
+import ProtectedComponent from "@/components/ProtectedComponent";
 
 interface Props {
   project: Project;
@@ -15,10 +17,23 @@ interface Props {
 function ProjectForm({ project, onSave }: Props): JSX.Element {
   const [currentProject, updateCurrentProject] = useState(project);
 
+  const { hasPermission: hasFinancialPermission } = usePermissions({
+    ability: abilities.change,
+    target: targets.financial,
+  });
+
   const sidenavItems = [
     { icon: "person", label: "profile", href: "profile" },
     { icon: "receipt_long", label: "basic info", href: "basic-info" },
   ];
+
+  if (hasFinancialPermission) {
+    sidenavItems.push({
+      icon: "receipt_long",
+      label: "contracts",
+      href: "contracts",
+    });
+  }
 
   return (
     <SidenavForm sidebarItems={sidenavItems}>
@@ -34,15 +49,18 @@ function ProjectForm({ project, onSave }: Props): JSX.Element {
           }}
         />
       </Grid>
-      <Grid item xs={12}>
-        <StatementsOfWork
-          project={currentProject}
-          onChange={updateCurrentProject}
-          onSave={() => {
-            onSave(currentProject);
-          }}
-        />
-      </Grid>
+
+      <ProtectedComponent ability={abilities.change} target={targets.financial}>
+        <Grid item xs={12}>
+          <StatementsOfWork
+            project={currentProject}
+            onChange={updateCurrentProject}
+            onSave={() => {
+              onSave(currentProject);
+            }}
+          />
+        </Grid>
+      </ProtectedComponent>
     </SidenavForm>
   );
 }
