d25cb0393db33bd6450d9d08144ae5269986a71d
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-11-07 16:23:53 -0300

    add live data to the metrics

---
+++ src/app/projects/[id]/_presenters/components/TimeEntries/_presenters/controllers/useTimeEntriesController.ts
@@ -0,0 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+
+import { getStatementOfWorks } from "@/app/projects/_presenters/components/ProjectForm/_presenters/components/StatementsOfWork/_presenters/data/services/statementsOfWork";
+
+import { getTimeEntriesAnalytics } from "../data/services/timeEntriesAnalytics";
+
+const useTimeEntriesController = (project: Project, startDate, endDate) => {
+  const { data: statementsOfWork, isLoading: isLoadingStatementsOfWork } =
+    useQuery({
+      queryKey: ["statements_of_work", project.id],
+      queryFn: () => getStatementOfWorks(project.id!),
+      enabled: !!project.id,
+    });
+
+  const statementOfWorkId = statementsOfWork?.[0]?.id;
+
+  const { data, isLoading } = useQuery({
+    queryKey: [
+      "analytics",
+      "time_entries",
+      statementOfWorkId,
+      startDate,
+      endDate,
+    ],
+    queryFn: () =>
+      getTimeEntriesAnalytics(statementOfWorkId, startDate, endDate),
+    enabled: !!statementOfWorkId,
+  });
+
+  return {
+    timeEntries: data,
+    isLoading,
+  };
+};
+
+export default useTimeEntriesController;
