d3bce15de0296b22e636a58df98f57ec181572db
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-11-22 17:31:41 -0300

    update time entries to depend on project only

--- src/components/Analytics/TimeEntries/_presenters/data/services/timeEntriesAnalytics.ts
+++ src/components/Analytics/TimeEntries/_presenters/data/services/timeEntriesAnalytics.ts
@@ -1,15 +1,15 @@
 import { backstageApiClient } from "@/app/_presenters/data/auth/backstageApiAxios";
 
 export const getTimeEntriesAnalytics = async (
-  statementOfWorkId: number,
-  startDate,
-  endDate
+  startDate: string,
+  endDate: string,
+  projectId: number | undefined
 ) => {
   const { data } = await backstageApiClient.get(
     `/analytics/time_entries.json`,
     {
       params: {
-        statement_of_work_id: statementOfWorkId,
+        project_id: projectId,
         start_date: startDate,
         end_date: endDate,
       },
