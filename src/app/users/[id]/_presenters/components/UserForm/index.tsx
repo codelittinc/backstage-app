42942bbb7f65954c444c01833e50a9def2b0a14d
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-01-22 13:13:15 -0300

    refactor user edit page

--- src/app/users/[id]/_presenters/components/UserForm/index.tsx
+++ src/app/users/[id]/_presenters/components/UserForm/index.tsx
@@ -1,10 +1,8 @@
 "use client";
 import Grid from "@mui/material/Grid";
-import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 import { User } from "@/app/_domain/interfaces/User";
-import FormLayout from "@/components/LayoutContainers/FormLayout";
 import Loading from "@/components/Loading";
 
 import Accounts from "./_presenters/Accounts";
@@ -12,11 +10,13 @@ import BasicInfo from "./_presenters/BasicInfo";
 import useUserFormController from "./_presenters/controllers/useUserFormController";
 import Header from "./_presenters/Header";
 
-function UserForm(): JSX.Element {
-  const { id } = useParams();
-  const { user, professions, isLoading, onSave } = useUserFormController(
-    id as string
-  );
+type Props = {
+  user: User;
+  onSave: (user: User) => void;
+};
+
+function UserForm({ user, onSave }: Props): JSX.Element {
+  const { professions, isLoading } = useUserFormController();
 
   const [editUser, setEditUser] = useState(user as User);
 
