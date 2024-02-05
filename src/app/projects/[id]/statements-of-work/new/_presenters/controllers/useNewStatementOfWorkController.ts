168b9662155f3c40267e0e2d1289b77385d564b0
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-02-05 08:49:13 -0300

    fix contract model form

--- src/app/projects/[id]/statements-of-work/new/_presenters/controllers/useNewStatementOfWorkController.ts
+++ src/app/projects/[id]/statements-of-work/new/_presenters/controllers/useNewStatementOfWorkController.ts
@@ -1,11 +1,15 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 
+import { StatementOfWork } from "@/app/_domain/interfaces/StatementOfWork";
 import { useAppStore } from "@/app/_presenters/data/store/store";
 import { createStatementOfWork } from "@/app/projects/_presenters/components/ProjectForm/_presenters/components/StatementsOfWork/_presenters/data/services/statementsOfWork";
+import routes from "@/routes";
 
 const useNewStatementsOfWorkController = (projectId: number | string) => {
   const { showSaveSuccessAlert } = useAppStore();
   const queryClient = useQueryClient();
+  const router = useRouter();
 
   const updateMutation = useMutation({
     mutationFn: createStatementOfWork,
@@ -18,6 +22,8 @@ const useNewStatementsOfWorkController = (projectId: number | string) => {
       queryClient.invalidateQueries({
         queryKey: ["statements_of_work", projectId],
       });
+
+      router.push(routes.statementOfWorkPath(result.id as number, projectId));
     },
   });
 
