1f8cce3ab1a61ede85f483ba5a1c29e221540bd1
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-02-05 13:32:42 -0300

    refactor text input controller

--- src/components/Form/FieldControllers/TextInputController/index.tsx
+++ src/components/Form/FieldControllers/TextInputController/index.tsx
@@ -1,49 +1,34 @@
-import { Controller } from "react-hook-form";
+import { Control, Controller } from "react-hook-form";
 
 import FormField from "@/components/FormField";
 
 type Props = {
   [key: string]: any;
-  control: any;
+  control: Control;
   label: string;
   name: string;
 };
 
-const TextInputController = ({
-  name,
-  control,
-  label,
-  placeholder,
-  required,
-  ...rest
-}: Props) => {
-  let rules = {};
-
-  if (required) {
-    rules = Object.assign(rules, { required: "This field is required" });
-  }
-
-  return (
-    <Controller
-      name={name}
-      control={control}
-      rules={rules}
-      render={({ field: { onChange, value }, fieldState: { error } }) => {
-        return (
-          <FormField
-            helperText={error ? error.message : null}
-            placeholder={placeholder}
-            error={!!error}
-            onChange={onChange}
-            value={value}
-            label={label}
-            required={required}
-            {...rest}
-          />
-        );
-      }}
-    />
-  );
-};
+const TextInputController = ({ name, control, required, ...rest }: Props) => (
+  <Controller
+    name={name}
+    control={control}
+    rules={{
+      required: required && "This field is required",
+    }}
+    render={({ field: { onChange, value }, fieldState: { error } }) => {
+      return (
+        <FormField
+          helperText={error ? error.message : null}
+          error={!!error}
+          onChange={onChange}
+          value={value}
+          required={required}
+          {...rest}
+        />
+      );
+    }}
+  />
+);
 
 export default TextInputController;
