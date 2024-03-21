af6a3421d3bea09823515ba2cb03e4537289bbfd
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-03-21 07:45:49 -0300

    fix performance metrics tanstack keys

--- src/app/users/[id]/_presenters/components/PerformanceMetrics/_presenters/components/UserPullRequests/_presenters/controllers/useUserPullRequestsController.ts
+++ src/app/users/[id]/_presenters/components/PerformanceMetrics/_presenters/components/UserPullRequests/_presenters/controllers/useUserPullRequestsController.ts
@@ -4,6 +4,7 @@ import { useParams } from "next/navigation";
 import tanstackKeys from "@/app/_domain/enums/tanstackKeys";
 import useUsersController from "@/app/_presenters/controllers/useUsersController";
 import { getPullRequests } from "@/app/projects/[id]/_presenters/components/PerformanceMetrics/_presenters/components/PullRequestsSection/_presenters/data/services/pullRequests";
+import { formatDateToMonthDayYear } from "@/app/_presenters/utils/date";
 
 const useUserPullRequestsController = (startDate: string, endDate: string) => {
   const { id: userId } = useParams();
@@ -13,7 +14,11 @@ const useUserPullRequestsController = (startDate: string, endDate: string) => {
   const user = users?.find((user) => user.slug === userId);
 
   const { data: pullRequests, isLoading: isLoadinPullRequests } = useQuery({
-    queryKey: [tanstackKeys.PullRequests, startDate, endDate],
+    queryKey: [
+      tanstackKeys.PullRequests,
+      formatDateToMonthDayYear(startDate),
+      formatDateToMonthDayYear(endDate),
+    ],
     queryFn: () =>
       getPullRequests({
         startDate: startDate,
