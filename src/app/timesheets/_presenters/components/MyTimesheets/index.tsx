d0d4aeb68657ba366f9396aaf46878e56bf9d956
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-03-29 17:20:57 -0300

    fix my time entries

--- src/app/timesheets/_presenters/components/MyTimesheets/index.tsx
+++ src/app/timesheets/_presenters/components/MyTimesheets/index.tsx
@@ -2,9 +2,16 @@ import useCurrentUserController from "@/app/_presenters/controllers/useCurrentUs
 import Loading from "@/components/Loading";
 
 import Timesheets from "../Timesheets";
+import useDateRangeController from "@/app/_presenters/controllers/queries/useDateRangeController";
+import { useEffect } from "react";
 
 const MyTimesheets = () => {
   const { currentUser, isLoading } = useCurrentUserController();
+  const { updateDateRangeQuery } = useDateRangeController();
+
+  useEffect(() => {
+    updateDateRangeQuery(new Date());
+  }, []);
 
   if (isLoading) {
     return <Loading />;
