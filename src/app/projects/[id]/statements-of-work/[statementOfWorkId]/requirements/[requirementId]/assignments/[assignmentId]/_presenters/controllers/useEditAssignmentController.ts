622c3ecc4ea4899dc9dc5587b7bc94cc156ab648
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-06-01 10:40:36 -0300

    add assingment delete

--- src/app/projects/[id]/statements-of-work/[statementOfWorkId]/requirements/[requirementId]/assignments/[assignmentId]/_presenters/controllers/useEditAssignmentController.ts
+++ src/app/projects/[id]/statements-of-work/[statementOfWorkId]/requirements/[requirementId]/assignments/[assignmentId]/_presenters/controllers/useEditAssignmentController.ts
@@ -2,18 +2,27 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 import tanstackKeys from "@/app/_domain/enums/tanstackKeys";
 import {
+  deleteAssignment,
   getAssignment,
   updateAssignment,
 } from "@/app/_presenters/data/assignments";
 import { getRequirement } from "@/app/_presenters/data/requirements";
 import { useAppStore } from "@/app/_presenters/data/store/store";
+import { useRouter } from "next/navigation";
+import routes from "@/routes";
+import projectTabs from "@/app/projects/_domain/_enums/projectTabs";
+import { useState } from "react";
+import { set } from "date-fns";
 
 const useEditAssignmentController = (
   assignmentId: string,
-  requirementId: string
+  requirementId: string,
+  projectId: string
 ) => {
   const { showSaveSuccessAlert } = useAppStore();
   const queryClient = useQueryClient();
+  const router = useRouter();
+  const [isDeleted, setIsDeleted] = useState(false);
 
   const updateMutation = useMutation({
     mutationFn: updateAssignment,
@@ -28,11 +37,21 @@ const useEditAssignmentController = (
   const { data, isLoading } = useQuery({
     queryKey: [tanstackKeys.Requirements, requirementId],
     queryFn: () => getRequirement(requirementId),
+    enabled: !isDeleted,
   });
 
   const { data: assignment, isLoading: isLoadingAssignment } = useQuery({
     queryKey: [tanstackKeys.Assignments, assignmentId],
     queryFn: () => getAssignment(assignmentId),
+  });
+
+  const deleteMutation = useMutation({
+    mutationFn: deleteAssignment,
+    onSuccess: async () => {
+      setIsDeleted(true);
+      router.push(routes.projectPath(projectId, projectTabs.resources));
+      showSaveSuccessAlert();
+    },
   });
 
   return {
@@ -42,6 +61,9 @@ const useEditAssignmentController = (
     assignment: assignment,
     requirement: data,
     isLoading: isLoading || isLoadingAssignment,
+    onDelete: (assignment: Assignment) => {
+      deleteMutation.mutate(assignment);
+    },
   };
 };
 
