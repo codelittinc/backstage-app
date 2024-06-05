87044c13734c8413ea8a2347f30abaad4b297a5c
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-06-05 19:33:15 -0300

    improve dashboard

--- src/app/project-dashboard/page.tsx
+++ src/app/project-dashboard/page.tsx
@@ -8,6 +8,10 @@ import Loading from "@/components/Loading";
 import { useEffect } from "react";
 import { useAppStore } from "../_presenters/data/store/store";
 import { DEFAULT_STATEMENT_OF_WORK } from "@/components/PageFilters/StatementOfWorkFilter";
+import { Grid, Icon, Typography } from "@mui/material";
+import Avatar from "@/components/Avatar";
+import Box from "@/components/Box";
+import Link from "next/link";
 
 const ProjectDashboard = () => {
   const params = useSearchParams();
@@ -32,10 +36,54 @@ const ProjectDashboard = () => {
   }
 
   return (
-    <TimeEntries
-      project={project}
-      defaultStatementOfWork={DEFAULT_STATEMENT_OF_WORK}
-    />
+    <Grid container>
+      <Grid item xs={12} padding={5}>
+        <Grid xs={12} justifyContent={"center"} display="flex" mb={3}>
+          <Avatar
+            src={project?.logoUrl}
+            bgColor={project?.logoBackgroundColor}
+          />
+          <Typography variant={"h2"} ml={2}>
+            {project.name}
+          </Typography>
+        </Grid>
+        <Grid item xs={12} justifyContent={"center"} display="flex">
+          <Grid item xs={10}>
+            <TimeEntries
+              project={project}
+              defaultStatementOfWork={DEFAULT_STATEMENT_OF_WORK}
+            />
+          </Grid>
+        </Grid>
+        <Box
+          mt={2}
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          flexWrap="wrap"
+          color="text"
+        >
+          <Typography variant="caption" fontWeight="medium">
+            &copy; {new Date().getFullYear()}, made with
+          </Typography>
+          <Typography variant="caption" fontWeight="medium">
+            <Box color="text" mb={-0.5} mx={0.25}>
+              <Icon color="error" fontSize="inherit">
+                favorite
+              </Icon>
+            </Box>
+          </Typography>
+          <Typography variant="caption" fontWeight="medium">
+            by
+          </Typography>
+          <Typography variant="caption" fontWeight="medium">
+            <Link href={"https://codelitt.com"} target="_blank">
+              &nbsp;Codelitt&nbsp;
+            </Link>
+          </Typography>
+        </Box>
+      </Grid>
+    </Grid>
   );
 };
 
