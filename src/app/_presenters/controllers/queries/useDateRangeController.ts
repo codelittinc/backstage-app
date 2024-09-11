f10b83dee00293736d9a195bc8046b94b90f9560
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-09-11 15:59:06 -0300

    update date range period

--- src/app/_presenters/controllers/queries/useDateRangeController.ts
+++ src/app/_presenters/controllers/queries/useDateRangeController.ts
@@ -1,7 +1,20 @@
+import { format } from "path";
 import useQueryParamController from "../useQueryParamController";
 
 const START_DATE_KEY = "startDate";
 const END_DATE_KEY = "endDate";
+
+const formatDate = (date: Date | undefined): string | undefined => {
+  if (!date) return undefined;
+  const formattedDate = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  }).format(date);
+
+  const [month, day, year] = formattedDate.split("/");
+  return `${year}-${month}-${day}`;
+};
 
 const useDateRangeController = (
   defaultStartDate?: Date,
@@ -10,26 +23,29 @@ const useDateRangeController = (
   const { setCustomParams, getCustomParamValue } = useQueryParamController();
 
   const updateDateRangeQuery = (startDate: Date, endDate?: Date) => {
+    const formattedStartDate = formatDate(startDate);
+    const formattedEndDate = endDate ? formatDate(endDate) : undefined;
+
     setCustomParams([
       {
         key: START_DATE_KEY,
-        value: startDate.toISOString(),
+        value: formattedStartDate,
       },
       {
         key: END_DATE_KEY,
-        value: endDate?.toISOString(),
+        value: formattedEndDate,
       },
     ]);
   };
 
   const startDate = getCustomParamValue(
     START_DATE_KEY,
-    defaultStartDate?.toISOString() ?? ""
+    formatDate(defaultStartDate) ?? ""
   ) as string;
 
   const endDate = getCustomParamValue(
     END_DATE_KEY,
-    defaultEndDate?.toISOString() ?? ""
+    formatDate(defaultEndDate) ?? ""
   ) as string;
 
   return {
