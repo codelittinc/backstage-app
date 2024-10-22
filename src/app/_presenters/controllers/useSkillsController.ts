47356e59c31fec0520ad4d3c3d22c5a46e1ea6c2
Alessandro <alessandromontividiu03@gmail.com>
2024-10-22 11:45:59 -0300

    Refactor Skills and UserSkills components

--- src/app/_presenters/controllers/useSkillsController.ts
+++ src/app/_presenters/controllers/useSkillsController.ts
@@ -1,19 +1,95 @@
 "use client";
-import { useQuery } from "@tanstack/react-query";
 
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { useFieldArray, useForm } from "react-hook-form";
+
+import skillLevelKeys from "@/app/_domain/enums/skillLevelKeys";
 import tanstackKeys from "@/app/_domain/enums/tanstackKeys";
+import { UserSkill } from "@/app/_domain/interfaces/Skill";
 
 import { getSkills } from "../data/skills";
+import { useAppStore } from "../data/store/store";
+import { getUserSkills, updateUserSkills } from "../data/userSkills";
 
-const useSkillsController = () => {
-  const { data, isLoading } = useQuery({
+const defaultUserSkill: UserSkill = {
+  lastAppliedYear: 2010,
+  level: skillLevelKeys.Beginner,
+  yearsOfExperience: 0,
+  skillId: undefined,
+  userId: undefined,
+};
+
+const useSkillsController = (id: number | string) => {
+  const queryClient = useQueryClient();
+  const { data: skills, isLoading: skillsLoading } = useQuery({
     queryKey: [tanstackKeys.Skills],
     queryFn: getSkills,
   });
 
+  const { data: userSkills, isLoading: userSkillsLoading } = useQuery({
+    queryKey: [tanstackKeys.UserSkills],
+    queryFn: () => {
+      return getUserSkills(id);
+    },
+  });
+
+  const { showSaveSuccessAlert } = useAppStore();
+
+  const mutation = useMutation({
+    mutationFn: ({
+      userSkills,
+      userId,
+    }: {
+      userId: number | string;
+      userSkills: UserSkill[];
+    }) => updateUserSkills(userSkills, userId),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [tanstackKeys.UserSkills],
+      });
+      showSaveSuccessAlert();
+    },
+  });
+
+  const { control, handleSubmit, reset } = useForm<{ userSkills: UserSkill[] }>(
+    {
+      defaultValues: {
+        userSkills: [defaultUserSkill],
+      },
+    }
+  );
+
+  const { fields, append, remove, replace } = useFieldArray({
+    control,
+    name: 'userSkills',
+  });
+
+  const onSubmit = (data: { userSkills: UserSkill[] }) => {
+    mutation.mutate({ userSkills: data.userSkills || [], userId: id });
+  };
+
+  const addNewSkillForm = () => {
+    append(defaultUserSkill);
+  };
+
+  useEffect(() => {
+    if (userSkills?.length) {
+      reset({ userSkills });
+      replace(userSkills);
+    }
+  }, [userSkills, replace, reset]);
+
   return {
-    skills: data,
-    isLoading: isLoading,
+    skills,
+    userSkills,
+    isLoading: skillsLoading || userSkillsLoading,
+    addNewSkillForm,
+    onSubmit,
+    handleSubmit,
+    fields,
+    remove,
+    control
   };
 };
 
--- src/app/users/_presenters/components/UserForm/_presenters/Skills/_presenters/controllers/useSkillFormController.ts
+++ src/app/users/_presenters/components/UserForm/_presenters/Skills/_presenters/controllers/useSkillFormController.ts
@@ -1,13 +1,24 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import { Skill } from '@/app/_domain/interfaces/Skill';
+import { Skill, UserSkill } from '@/app/_domain/interfaces/Skill';
 
-const useSkillFormController = () => {
+interface Props {
+  skills?: Skill[] | null;
+  userSkill?: UserSkill;
+}
+
+const useSkillFormController = ({ skills, userSkill }: Props) => {
   const [selectedSkill, setSelectedSkill] = useState<Skill | null>(null);
 
+  useEffect(() => {
+    if (skills && userSkill?.skillId) {
+      const skill = skills.find((s) => s.id === userSkill.skillId);
+      setSelectedSkill(skill || null);
+    }
+  }, [skills, userSkill, setSelectedSkill]);
+
   return {
-    selectedSkill,
-    setSelectedSkill,
+    selectedSkill
   };
 };
 
