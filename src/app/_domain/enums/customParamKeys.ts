4aab83b8e7d0d28ab354fec2ecd4b4bb2d5f80ce
Kaio Magalhaes <me@kaiomagalhaes.com>
2025-06-25 15:02:24 -0300

    persist filters in the url for the users table

--- src/app/_domain/enums/customParamKeys.ts
+++ src/app/_domain/enums/customParamKeys.ts
@@ -1,5 +1,10 @@
 enum customParamKeys {
   statementOfWorkId = "statementOfWorkId",
+  // User filter keys
+  userFilterActive = "filterActive",
+  userFilterInternal = "filterInternal",
+  userFilterRehireable = "filterRehireable",
+  userSelectedProfessions = "selectedProfessions",
 }
 
 export default customParamKeys;
